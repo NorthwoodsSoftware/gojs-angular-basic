eaa9c7bdd6320ffa87af06607e88281c0a4f44d7
Simon Sarris <simonsarris@users.noreply.github.com>
2026-01-08 11:46:03 -0500

    update gojs-angular, remove zones

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,6 @@
 import { ApplicationConfig } from '@angular/core';
+import { provideZonelessChangeDetection } from '@angular/core';
 
 export const appConfig: ApplicationConfig = {
-  providers: [],
+  providers: [provideZonelessChangeDetection()],
 };
